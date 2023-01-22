793e9fe1a39364f4d52d3f225b88c96caac0e719
Hafez <hafez.shabrang@yahoo.com>
2023-01-22 11:48:55 +0330

    testing category models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ApplicationRecord
-  validates :name, presence: true, length: {minimum: 3, maximum: 25}
+  validates :name, presence: true, length: {minimum: 3, maximum: 25}, uniqueness: true
 end
--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @category = Category.new(name: "Sports")
+  end
+
+  test "Category must be valid" do
+    assert @category.valid?
+  end
+
+  test "Category must be present" do
+    @category.name = " "
+    assert_not @category.valid?
+  end
+
+  test "Category minimum character must be 3" do
+    @category.name = "aa"
+    assert_not @category.valid?
+  end
+
+  test "Category maximum character must be 25" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "Category name must be unique" do
+    @category.save
+    @category2 = Category.new(name: "Sports")
+    assert_not @category2.valid?
+  end
+
 end
