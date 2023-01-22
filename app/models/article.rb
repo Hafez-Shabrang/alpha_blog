7c9f2bc1974359fbda28e190aa1b50669fa15203
Hafez <hafez.shabrang@yahoo.com>
2023-01-22 15:06:39 +0330

    add articlecategories model and end of category testing controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   paginates_per 5
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true, length: {minimum: 4, maximum: 20}
   validates :description, presence: true, length: {minimum: 5, maximum: 1000}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,6 @@ Rails.application.routes.draw do
 
   get "logout", to: "sessions#destroy"
 
+  resources :categories, only: [:index, :show, :new, :create]
+
 end
